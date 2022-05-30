f87cf5cf8058107117811bc35f1ae447db04feff
devzer01 <nayana.hettiarachchi@hiacuity.com>
2022-05-30 09:31:20 +0700

    second activity

---
+++ app/src/main/java/io/cognetic/geoquiz/ActivityCheat.kt
@@ -0,0 +1,11 @@
+package io.cognetic.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityCheat : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+    }
+}
--- app/src/main/java/io/cognetic/geoquiz/MainActivity.kt
+++ app/src/main/java/io/cognetic/geoquiz/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.cognetic.geoquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
     private lateinit var toast : Toast
 
@@ -37,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
@@ -50,6 +53,11 @@ class MainActivity : AppCompatActivity() {
         nextButton.setOnClickListener {
             quizViewModel.moveToNext()
             updateQuestion()
+        }
+
+        cheatButton.setOnClickListener {
+            val intent = Intent(this, ActivityCheat::class.java)
+            startActivity(intent)
         }
 
         updateQuestion()
